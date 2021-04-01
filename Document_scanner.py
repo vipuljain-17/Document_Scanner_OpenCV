ad75bf9e58f15fc08d85dff80bdea06eeb040788
Vipul Jain <vipul.today@gmail.com>
2021-04-02 03:22:23 +0530

    Added frontend to the scanner

--- Document_scanner.py
+++ Document_scanner.py
@@ -18,76 +18,80 @@ def projected_image_size(pts):
 
     return (dst,maxWidth,maxHeight)
 
-image = cv2.imread('Sample.jpg')  #take input of image
-org = image.copy()
-cv2.imshow("Orignal",org)
-cv2.waitKey(0)
+path = 'Sample_resized.jpg'
 
-#Resize the image for better and fast processing
-r = 600.0 / image.shape[1]
-dim = (int(image.shape[1]*r),600)
+def scanner(file_path):
+    image = cv2.imread(file_path)  #take input of image
+    org = image.copy()
+    #cv2.imshow("Orignal",org)
+    #cv2.waitkey(0)
 
-image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    #Resize the image for better and fast processing
+    r = 600.0 / image.shape[1]
+    dim = (int(image.shape[1]*r),600)
 
-image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-cv2.imshow("GrayScaled",image)
-cv2.waitKey(0)
+    image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
 
-image = cv2.bilateralFilter(image, 5, 21, 21) #Blur the image for better edge detection
-cv2.imshow("Blurred",image)
-cv2.waitKey(0)
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    #cv2.imshow("GrayScaled",image)
+    #cv2.waitkey(0)
 
-#Edge detection using canny algorithm
-edged = cv2.Canny(image,30,160)
-cv2.imshow("Edged",edged)
-cv2.waitKey(0)
+    image = cv2.bilateralFilter(image, 5, 21, 21) #Blur the image for better edge detection
+    #cv2.imshow("Blurred",image)
+    #cv2.waitkey(0)
 
-(contours, _) = cv2.findContours(edged, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    #Edge detection using canny algorithm
+    edged = cv2.Canny(image,30,160)
+    #cv2.imshow("Edged",edged)
+    #cv2.waitkey(0)
 
-'''Arrange the list of contours found in descending order
-  in the basis of contour area'''
-contours.sort(key=cv2.contourArea, reverse=True)
+    (contours, _) = cv2.findContours(edged, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-img = image.copy()
-cv2.drawContours(img,contours,-1,color=(0,0,255),thickness = 3)
-cv2.imshow("Contoured image",img)
-cv2.waitKey(0)
+    '''Arrange the list of contours found in descending order
+    in the basis of contour area'''
+    contours.sort(key=cv2.contourArea, reverse=True)
 
-'''Find the main contour that is our main document'''
-for c in contours:
-    arc_length = 0.1*cv2.arcLength(c,True)  #Find the perimeter of closed curve
-    no_of_points_found = cv2.approxPolyDP(c,arc_length,True)
+    img = image.copy()
+    cv2.drawContours(img,contours,-1,color=(0,0,255),thickness = 3)
+    #cv2.imshow("Contoured image",img)
+    #cv2.waitkey(0)
 
-    if len(no_of_points_found) == 4:
-        Main_contour = no_of_points_found
-        break
-copy = img.copy()
-img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-cv2.drawContours(img,[Main_contour],-1,(0,255,0),2)
-cv2.imshow("Main outline",img)
-cv2.waitKey(0)
+    '''Find the main contour that is our main document'''
+    for c in contours:
+        arc_length = 0.1*cv2.arcLength(c,True)  #Find the perimeter of closed curve
+        no_of_points_found = cv2.approxPolyDP(c,arc_length,True)
 
-#Get those four points and crop the image
-tl = Main_contour[0][0]
-tr = Main_contour[3][0]
-bl = Main_contour[1][0]
-br = Main_contour[2][0]
+        if len(no_of_points_found) == 4:
+            Main_contour = no_of_points_found
+            break
+    copy = img.copy()
+    img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(img,[Main_contour],-1,(0,255,0),2)
+    #cv2.imshow("Main outline",img)
+    #cv2.waitkey(0)
 
-pts = (tl,tr,bl,br)
-pts1 = np.float32([tl,tr,bl,br])
+    #Get those four points and crop the image
+    tl = Main_contour[0][0]
+    tr = Main_contour[3][0]
+    bl = Main_contour[1][0]
+    br = Main_contour[2][0]
 
-(pts2,maxWidth,maxHeight) = projected_image_size(pts1)
+    pts = (tl,tr,bl,br)
+    pts1 = np.float32([tl,tr,bl,br])
 
-matrix = cv2.getPerspectiveTransform(pts1,pts2)
-cropped = cv2.warpPerspective(image, matrix,(maxWidth,maxHeight))
-cv2.imshow("Cropped",cropped)
-cv2.waitKey(0)
+    (pts2,maxWidth,maxHeight) = projected_image_size(pts1)
 
-#For that scanned black and white look
-T = threshold_local(cropped, 13, offset = 10, method = "gaussian")
-cropped = (cropped > T).astype("uint8") * 255
-cv2.imshow("Scanned",cropped)
-cv2.waitKey(0)
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    cropped = cv2.warpPerspective(image, matrix,(maxWidth,maxHeight))
+    #cv2.imshow("Cropped",cropped)
+    #cv2.waitkey(0)
 
-print("Image Scanned Successfully")
-cv2.destroyAllWindows()
+    #For that scanned black and white look
+    T = threshold_local(cropped, 13, offset = 10, method = "gaussian")
+    cropped = (cropped > T).astype("uint8") * 255
+    #cv2.imshow("Scanned",cropped)
+    #cv2.waitkey(0)
+
+    #print("Image Scanned Successfully")
+    #cv2.destroyAllWindows()
+    return cropped
---
+++ frontend.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import tkinter
+from tkinter.filedialog import askopenfilename
+from tkinter import Button, Label
+from PIL import ImageTk, Image
+import cv2
+from Document_scanner import scanner
+
+root = tkinter.Tk()
+
+
+def open_file():
+    file_path = askopenfilename(title = "Select File", filetypes =[('Image File', '*.jpeg *.jpg *.png')])
+    if file_path is not None:
+        return file_path
+    else:
+        pass
+
+def open_img():
+    global panelA, panelB
+
+    file_path = open_file()
+
+    image = cv2.imread(file_path)
+    scanned = scanner(file_path)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (600,600), interpolation=cv2.INTER_NEAREST)
+
+    image = Image.fromarray(image)
+    scanned = Image.fromarray(scanned)
+
+    image = ImageTk.PhotoImage(image)
+    scanned = ImageTk.PhotoImage(scanned)
+
+    if panelA is None or panelB is None:
+        panelA = Label(image=image)
+        panelA.image = image
+        panelA.pack(side="left",padx=10, pady=10)
+
+        panelB = Label(image=scanned)
+        panelB.image = scanned
+        panelB.pack(side="left",padx=10, pady=10)
+    
+    else:
+        panelA.configure(image=image)
+        panelB.configure(image=scanned)
+        panelA.image = image
+        panelB.image = scanned
+
+panelA = None
+panelB = None
+btn = Button(root, text ='Select an Image', command = open_img)
+btn.pack(side="bottom",fill="both", expand="yes", padx="10", pady="10")
+
+
+root.title("Document Scanner Using OpenCV") 
+#root.geometry("700x700")
+root.mainloop()
